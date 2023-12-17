a989c7ea38fc2a048503ec3ee67a93f6b61696e2
sima <38819603+sima01@users.noreply.github.com>
2023-12-17 17:57:23 -0500

    Update main.tf

--- terraform2023/main.tf
+++ terraform2023/main.tf
@@ -6,11 +6,11 @@ resource "google_project" "my_project" {
 
 
 resource "google_storage_bucket" "bucket" {
-  project =  google_project.my_project.id
-  name  = "dragonfly202343"
-  location = "US"
+  project       =  google_project.my_project.id
+  name          = "dragonfly202343"
+  location      = "US"
   storage_class = "STANDARD"
-  depends_on = [
+  depends_on    = [
      google_project.my_project
 ]
 }
