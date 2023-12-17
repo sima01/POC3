35b54639ce8a96694aa4c6da0a604a71f3152a4f
sima <38819603+sima01@users.noreply.github.com>
2023-12-17 17:10:08 -0500

    Update main.tf

--- terraform2023/main.tf
+++ terraform2023/main.tf
@@ -1,4 +1,10 @@
 
+resource "google_project" "my_project" {
+  name       = "My Terraform Project "
+  project_id = "MyTerraformProject"
+}
+
+
 resource "google_storage_bucket" "bucket" {
   count =1
   name  = "dragonfly202333"
