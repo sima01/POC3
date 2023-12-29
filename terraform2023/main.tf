f1cfe5b733a13fbdd4dee1357ea4ebe7577ba8e2
sima <38819603+sima01@users.noreply.github.com>
2023-12-29 15:21:56 -0500

    Update main.tf

--- terraform2023/main.tf
+++ terraform2023/main.tf
@@ -1,12 +1,12 @@
 
 resource "google_project" "my_project" {
   name       = "My Terraform Project"
-  project_id =  var.project-for-bucket
+  project_id =  var.project_for_bucket
 }
 
 
 resource "google_storage_bucket" "buckett" {
-  project       = var.project-for-bucket
+  project       = var.project_for_bucket
   name          = "dragonfly202343"
   location      = "US"
   storage_class = "STANDARD"
